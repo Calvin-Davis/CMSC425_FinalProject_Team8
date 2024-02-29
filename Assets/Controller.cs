cfcb87c6f36c225a780bf0d726a74647fa0a60ea
jacob jarrell <jjarrell@terpmail.umd.edu>
2024-02-29 13:39:39 -0500

    basic movement finished, buggy gravity added

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -2,23 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Controller : MonoBehaviour
 {
     private CharacterController _controller;
-
+    // public float sensX;
+    // public float sensY;
+    // public Transform orientation;
+    // float xRotation;
+    // float yRotation;
+    private float ySpeed;
+    private bool flip;
+    [SerializeField] public float jumpSpeed = 10;
     [SerializeField] private float _speed = 5;
     // Start is called before the first frame update
     void Start()
     {
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;
+        UnityEngine.Cursor.visible = false;
         _controller = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0, Input.GetAxis("Vertical"));
+        //float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        //float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
+        // yRotation += mouseX;
+        // xRotation += mouseY;
+        // xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        // orientation.rotation = Quaternion.Euler(0, yRotation, 0);   
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if(Input.GetKeyDown("q")){
+            flip = !flip;
+        }
+        if(_controller.isGrounded) {
+            
+            if(!flip) {
+                if(Input.GetKeyDown("space")) {
+                    ySpeed = jumpSpeed;
+                }
+                
+            } else {
+                if(Input.GetKeyDown("space")) {
+                    ySpeed = -jumpSpeed;
+                }
+            }
+
+        } else if (!flip) {
+            ySpeed += Physics.gravity.y * Time.deltaTime;
+        } else {
+            ySpeed -= Physics.gravity.y * Time.deltaTime;
+        } 
+        move.y = ySpeed;
         _controller.Move(move * Time.deltaTime * _speed);
+        
     }
 
 }
