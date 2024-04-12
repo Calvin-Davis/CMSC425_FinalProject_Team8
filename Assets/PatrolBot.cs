9ca6a06494b60aceaa39ee83f856303e0cc94767
Jacob Jarrell <jjarrell@terpmail.umd.edu>
2024-04-12 14:34:33 -0400

    added isCyclic option which allows for either going back to starting goal at end or just turning around

--- Assets/PatrolBot.cs
+++ Assets/PatrolBot.cs
@@ -8,6 +8,8 @@ public class PatrolBot : MonoBehaviour
     public Transform[] goals;
     private int idx;
     private NavMeshAgent agent;
+    public bool isCyclic = true;
+    private int inverse = 1;
     
     
     // Start is called before the first frame update
@@ -23,10 +25,20 @@ public class PatrolBot : MonoBehaviour
     {
         
         if(Mathf.Abs(transform.position.x - goals[idx].position.x) <= 0.1 && Mathf.Abs(transform.position.z - goals[idx].position.z) <= 0.1) {
-            idx += 1;
+            idx += 1*inverse;
             
-            if (idx >= goals.Length) {
-                idx = 0;
+            if (idx == goals.Length) {
+                if(isCyclic) {
+                    idx = 0;
+                } else {
+                    idx -= 1; 
+                    inverse = -1;
+                }
+                
+            }
+            if(idx < 0) {
+                idx = 1;
+                inverse = 1;
             }
             agent.destination = goals[idx].position;
             Debug.Log(goals[idx].position);
