267d728f27487cb79c9850690ede6ff5cc1a435c
Jacob Jarrell <jjarrell@terpmail.umd.edu>
2024-04-19 15:00:10 -0400

    added single flipping fixed the no push grav cube by actually putting the changes made on the cc prefab so it works on levels other than just the test one. Note lvl now takes the whole cc not just the model

--- Assets/Scripts/Level1.cs
+++ Assets/Scripts/Level1.cs
@@ -10,8 +10,9 @@ public class Level1 : MonoBehaviour
     public string nameOfNextLevel;
 
     public bool flip = false;
-    public Transform playerModel;
+    public CharacterController playerModel;
     [SerializeField] public float _speed = 5;
+    int flipCount = 0;
 
     // Add reference to our CamController script
     public CamController camController;
@@ -26,9 +27,13 @@ public class Level1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("q")){
+        if(playerModel.isGrounded || ((playerModel.collisionFlags & CollisionFlags.Above) != 0)){
+            flipCount = 0;
+        }
+        if(Input.GetKeyDown("q") && flipCount == 0){
+            flipCount += 1;
             flip = !flip;
-            playerModel.localEulerAngles = new Vector3(180+ playerModel.localEulerAngles.x, 180 + playerModel.localEulerAngles.y, playerModel.localEulerAngles.z);
+            playerModel.transform.localEulerAngles = new Vector3(180+ playerModel.transform.localEulerAngles.x, 180 + playerModel.transform.localEulerAngles.y, playerModel.transform.localEulerAngles.z);
             camController.swapCams();
         }
     }
