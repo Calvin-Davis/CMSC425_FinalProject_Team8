8b8e8b2a886acf71dbb716ed19d94276ff55e854
Jacob Jarrell <jjarrell@terpmail.umd.edu>
2024-04-21 15:08:50 -0400

    added a spike repeater and an empty repeater which allows for the repetition of any object. To use put gameObject to be repeated in the bottom left corner of where you want grid to be and set repeated object to that game object (this is already done for you in spike repeater

---
+++ Assets/ObjectRepeater.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ObjectRepeater : MonoBehaviour
+{
+    public int rows;
+    public int cols;
+    public GameObject repeatedObject;
+
+
+
+    void Start()
+    {   
+        Debug.Log("Started");
+        Vector3 place = new Vector3(repeatedObject.transform.position.x, repeatedObject.transform.position.y, repeatedObject.transform.position.z);
+        Vector3 startingPoint = new Vector3(repeatedObject.transform.position.x, repeatedObject.transform.position.y, repeatedObject.transform.position.z);
+        for (int row = 0; row < rows; row++)
+        {
+            place.z = startingPoint.z + (row - (rows - 1) / 2f);
+            //Debug.Log(row);
+            for (int col = 0; col < cols; col++)
+            {
+                
+                place.x = startingPoint.x + (col - (cols - 1) / 2f);
+                
+                Instantiate<GameObject>(repeatedObject, place, Quaternion.identity);
+            }
+        }
+
+
+
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
