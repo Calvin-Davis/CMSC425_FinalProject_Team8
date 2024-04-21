b550979f17fd439bd4a9cd778942352579cc7ef0
Jacob Jarrell <jjarrell@terpmail.umd.edu>
2024-04-21 15:14:48 -0400

    added spacing variable to repeater to make it possible to increase gapes in grid. used if you want to have gaps or use on different sized gameobject

--- Assets/ObjectRepeater.cs
+++ Assets/ObjectRepeater.cs
@@ -8,7 +8,7 @@ public class ObjectRepeater : MonoBehaviour
     public int rows;
     public int cols;
     public GameObject repeatedObject;
-
+    public float spacing = 1.0f;
 
 
     void Start()
@@ -18,12 +18,12 @@ public class ObjectRepeater : MonoBehaviour
         Vector3 startingPoint = new Vector3(repeatedObject.transform.position.x, repeatedObject.transform.position.y, repeatedObject.transform.position.z);
         for (int row = 0; row < rows; row++)
         {
-            place.z = startingPoint.z + (row - (rows - 1) / 2f);
+            place.z = startingPoint.z + spacing * (row - (rows - 1) / 2f);
             //Debug.Log(row);
             for (int col = 0; col < cols; col++)
             {
                 
-                place.x = startingPoint.x + (col - (cols - 1) / 2f);
+                place.x = startingPoint.x + spacing * (col - (cols - 1) / 2f);
                 
                 Instantiate<GameObject>(repeatedObject, place, Quaternion.identity);
             }
