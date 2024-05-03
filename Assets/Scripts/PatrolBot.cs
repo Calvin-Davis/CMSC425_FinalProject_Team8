c04eb0e2ddfb582f2a6feed11d8d41443a5d19d5
Thomas Edwards <tedwardszony@gmail.com>
2024-05-03 12:52:30 -0400

    Add floating animation for patrol bots

--- Assets/Scripts/PatrolBot.cs
+++ Assets/Scripts/PatrolBot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Rendering;
@@ -7,14 +8,14 @@ using UnityEngine.AI;
 public class PatrolBot : MonoBehaviour
 {
     public Transform[] goals;
-    private int idx;
+    private int idx, inverse = 1;
     private NavMeshAgent agent;
     private Renderer eyeRenderer;
     public bool isCyclic = true;
-    private int inverse = 1;
-    public float botSpeed = 5;
+    private float animationTime = 0;
+    public float botSpeed = 5, floatHeight = 0.4F, floatAnimationSpeed = 3F;
     public Material eyeColor;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,23 +34,42 @@ public class PatrolBot : MonoBehaviour
     {
         //Walks to set node, once arives target changes to next node
         //cyclic decides whether to go back to first node at end or turn around at last node
-        if(Mathf.Abs(transform.position.x - goals[idx].position.x) <= 0.1 && Mathf.Abs(transform.position.z - goals[idx].position.z) <= 0.1) {
-            idx += 1*inverse;
-            
-            if (idx == goals.Length) {
-                if(isCyclic) {
+        if (Mathf.Abs(transform.position.x - goals[idx].position.x) <= 0.1 && Mathf.Abs(transform.position.z - goals[idx].position.z) <= 0.1)
+        {
+            idx += 1 * inverse;
+
+            if (idx == goals.Length)
+            {
+                if (isCyclic)
+                {
                     idx = 0;
-                } else {
-                    idx -= 1; 
+                }
+                else
+                {
+                    idx -= 1;
                     inverse = -1;
                 }
-                
+
             }
-            if(idx < 0) {
+            if (idx < 0)
+            {
                 idx = 1;
                 inverse = 1;
             }
             agent.destination = goals[idx].position;
         }
+
+        animateFloating();
+    }
+
+    void animateFloating()
+    {
+        float yPos = floatHeight * (Mathf.Cos(floatAnimationSpeed * animationTime + MathF.PI) + 1);
+        transform.position = new Vector3(transform.position.x, transform.position.y + yPos, transform.position.z);
+
+        if (animationTime >= 2 * MathF.PI / floatAnimationSpeed)
+            animationTime = 0;
+
+        animationTime += Time.deltaTime;
     }
 }
